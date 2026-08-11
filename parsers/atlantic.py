b41e5ca6ca0a8b0a45897f57fcfd8bf36cae9ece
Stephen <stephen@example.com>
2026-08-11 07:21:12 -0700

    Add manual make-up dates for Atlantic Crossword

--- parsers/atlantic.py
+++ parsers/atlantic.py
@@ -37,8 +37,27 @@ class AtlanticCrosswordParser(GameParser):
         total_seconds = minutes * 60 + seconds
         
         if total_seconds > 0:
-            # We don't have a puzzle ID in the text, so use the message date
-            puzzle_id = message_date.strftime("%Y-%m-%d")
+            # Check for a manual date in the text like 8/10, 08-10, 8/10/26, 8/10/2026
+            date_match = re.search(r'\b(1[0-2]|0?[1-9])[-/](3[01]|[12][0-9]|0?[1-9])(?:[-/](\d{4}|\d{2}))?\b', text)
+            if date_match:
+                month = int(date_match.group(1))
+                day = int(date_match.group(2))
+                year_str = date_match.group(3)
+                
+                if year_str:
+                    if len(year_str) == 2:
+                        year = 2000 + int(year_str)
+                    else:
+                        year = int(year_str)
+                else:
+                    # Default to current year based on message_date
+                    year = message_date.year
+                    
+                puzzle_id = f"{year:04d}-{month:02d}-{day:02d}"
+            else:
+                # We don't have a puzzle ID in the text, so use the message date
+                puzzle_id = message_date.strftime("%Y-%m-%d")
+                
             return puzzle_id, float(total_seconds)
             
         return None
